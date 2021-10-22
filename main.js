9ab4f923d4094e41790c64ef695627b8c6b60603
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-22 14:50:07 +0200

    add all click

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-const hamburgerIcon = document.querySelector('#hamburger-icon');
+const hamburgerIcon = document.querySelector('.header-right > a');
+hamburgerIcon.classList.add('hamburger-icon')
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const close = document.querySelector('.close');
 
 hamburgerIcon.addEventListener('click', function() {
-    console.log('HAMBURGER');
+    hamburgerMenu.classList.add('active');
+}); 
+
+close.addEventListener('click', function() {
+    hamburgerMenu.classList.remove('active');
 });
