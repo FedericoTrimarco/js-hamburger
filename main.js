13804bf3d76b963ceff69a2565723211012033b8
FedericoTrimarco <trimarcofederico98@gmail.com>
2021-10-22 14:51:20 +0200

    fix-1

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const hamburgerIcon = document.querySelector('.header-right > a');
-hamburgerIcon.classList.add('hamburger-icon')
+hamburgerIcon.classList.add('hamburger-icon');
+
 const hamburgerMenu = document.querySelector('.hamburger-menu');
+
 const close = document.querySelector('.close');
 
+        //  CLICK ON HAMBURGER-ICON
 hamburgerIcon.addEventListener('click', function() {
     hamburgerMenu.classList.add('active');
 }); 
-
+        //  CLICK ON CLOSE
 close.addEventListener('click', function() {
     hamburgerMenu.classList.remove('active');
 });
